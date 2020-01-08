ba4905ebd036722d5d569c9ac8d1cdef374685e4
Marcel <nuzzo.marcel@aliceadsl.fr>
2020-01-08 19:01:58 +0100

    first commit

---
+++ personnage.class.js
@@ -0,0 +1,25 @@
+﻿class Personnage {
+	constructor(ligne,colonne,imagesrc,nom) {
+		this.nom=nom;
+		this.ligne=ligne;
+		this.colonne=colonne;		
+		this.img=document.createElement("img");
+		this.img.src=imagesrc;
+		this.img.style.width="30px";
+		this.img.style.height="30px";		
+		this.img.alt=nom;
+		let oTd=oTable.rows[ligne].cells[colonne];
+		oTd.appendChild(this.img);
+	}
+
+	deplacer(newligne,newcolonne) {
+		let oTd=oTable.rows[this.ligne].cells[this.colonne];
+		oTd.innerHTML="";
+		this.ligne=newligne;
+		this.colonne=newcolonne;
+		oTd=oTable.rows[this.ligne].cells[this.colonne];
+		//oTd.innerHTML="";
+		oTd.appendChild(this.img);
+		//oTd.innerHTML="";
+	}
+}
